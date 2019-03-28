37cfbf766cc1deb8538d47852b416c5fb0d7f2bf
tnsihy <tnsihy@foxmail.com>
2019-03-28 13:25:13 +0800

    travel

--- travelTest/src/components/Header.vue
+++ travelTest/src/components/Header.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="header">
-      <div class="header-left"><span class="iconfont">&#xe624;</span></div>
-      <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题</div>
-      <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -15,29 +21,30 @@ export default {
 </script >
 
 <style lang="stylus" scoped>
-@import '../../assets/styles/varibles.styl'
+@import '~styles/varibles.styl'
+
 .header
   display flex
-  line-height:.86rem
-  background:$bgColor
-  color:#fff
+  line-height 0.86rem
+  background $bgColor
+  color #fff
   .header-left
-    width:.64rem
-    float:left
+    width 0.64rem
+    float left
   .header-input
     flex 1
-    margin-top .12rem
-    height .64rem
-    line-height .64rem
-    margin-left .2rem
+    margin-top 0.12rem
+    height 0.64rem
+    line-height 0.64rem
+    margin-left 0.2rem
     background #fff
-    border-radius .1rem
+    border-radius 0.1rem
     color #ccc
     text-align left
-    padding-left .2rem
+    padding-left 0.2rem
   .header-right
     width 1.24rem
     float right
     .arrow-icon
-      font-size .24rem
+      font-size 0.24rem
 </style>
