bb5d096834d2246069fbd32c37fc860fde66ba4c
tnsihy <tnsihy@foxmail.com>
2019-03-30 13:49:31 +0800

    city-search

--- travelTest/src/router/index.js
+++ travelTest/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import City from '@/components/city/City'
+import City from '@/components/City'
 
 Vue.use(Router)
 
