043ab7babea1f4cce76d14cd344064f6b0b68f03
tnsihy <tnsihy@foxmail.com>
2019-04-09 23:48:49 +0800

    Banner finish

--- travelTest/src/router/index.js
+++ travelTest/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import City from '@/components/City'
-
+import Detail from '@/components/Detail'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +16,11 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
