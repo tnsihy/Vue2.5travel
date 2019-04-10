1c1e379de9d1d3179443b99d4c2f64b19df538be
tnsihy <tnsihy@foxmail.com>
2019-04-10 22:25:45 +0800

    add animation

---
+++ travelTest/src/components/common/fade/FadeAnimation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <transition>
+            <!-- 大图渐隐动画 -->
+            <slot></slot>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FadeAnimation'
+}
+</script>
+
+<style lang="stylus" scoped>
+// 渐隐渐显
+.v-enter,.v-leave-to
+    opacity 0
+.v-enter-active,.v-leave-active
+    transition opacity .5s
+</style>
--- travelTest/src/components/detail/Banner.vue
+++ travelTest/src/components/detail/Banner.vue
@@ -13,12 +13,16 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleBannerClose"></common-gallary>
+    <fade-animation>
+      <!-- common-gallary作为一插槽插入fade-animation -->
+      <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleBannerClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from '@/components/common/gallary/Gallary'
+import FadeAnimation from '@/components/common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -32,7 +36,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   methods: {
     handleBannerClick: function () {
