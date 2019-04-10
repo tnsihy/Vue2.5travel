b99a75068830e51a5623b583cc11d45782b60394
tnsihy <tnsihy@foxmail.com>
2019-04-10 16:39:01 +0800

    详情页面

--- travelTest/src/components/Detail.vue
+++ travelTest/src/components/Detail.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from '@/components/detail/Banner'
+import DetailHeader from '@/components/detail/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.content
+  // 撑开页面的高度
+  height 50rem
 </style>
